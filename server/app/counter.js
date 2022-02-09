8c38b10eb4de8be6e88b22d4e2033d8720e27f55
melanydu94 <63240176+melanydu94@users.noreply.github.com>
2022-02-09 14:59:53 +0100

    Update counter.js

--- server/app/counter.js
+++ server/app/counter.js
@@ -1,1 +1,9 @@
-test
+const session = require('expression-session');
+  app.use(
+    session({
+      secret: 'secret string',
+      resave: false,
+      saveUninitialized: false, 
+      cookie: { /k can add cookie related info here */ }
+    })
+   );
