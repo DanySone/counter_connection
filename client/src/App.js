6133165d5c7ed6d42b1c6b2437f0ccd2caf69a60
melanydu94 <63240176+melanydu94@users.noreply.github.com>
2022-02-09 15:07:34 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,8 @@
-import logo from './logo.svg';
-import { useEffect } from 'react';
-import './App.css';
-
-function App() {
-  useEffect(() => {
-    fetch("/model").then(response =>
-      console.log(response)
-      ).then(jsonData => {
-        console.log(jsonData)
-      })
-  }, [])
-  
-  return (
-    
-const session = require('expression-session');
-  app.use(
-    session({
-      secret: 'secret string',
-      resave: false,
-      saveUninitialized: false, 
-      cookie: { /k can add cookie related info here */ }
-    })
-   );
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>
-          <h2>Counter</h2>
-        </div>
-      </header>
-      <footer>
-        <p>Author: Melany Ebrahim, Dany Sonethavy</p>
-        <p>The application is a counter which count the number of times the page has been accessed by the current use.</p>
-      </footer>
-    </div>
-  );
-}
-
-export default App;
-
+app.get('/', function(res, res){
+  if(!req.session.pageCountByCurrentUserOrAnyNameYouWant)
+    req.session.pageCountByCurrentUserOrAnyNameYouWant = 0;
+  req.session.pageCountByCurrentUserOrAnyNameYouWant++;
+  res.send({ 
+    pageCount: req.session.pageCountByCurrentUserOrAnyNameYouWant
+  });
+});
